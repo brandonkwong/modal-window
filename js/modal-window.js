e0e16de11dd0bc712b7148ca2ab97c56a360e61e
Brandon Kwong <brandonjk@gmail.com>
2014-06-09 00:46:44 -0700

    Updated modal-window.js
    Added Esc keydown event.

--- js/modal-window.js
+++ js/modal-window.js
@@ -4,7 +4,7 @@ URI: http://brandonkwong.com/dev/modal-window
 Description: An interactive modal window with form content, built to demonstrate various front-end technologies and exercise UI/IxD.
 Author: Brandon Kwong
 Author URI: http://brandonkwong.com
-Version: 1.0.0 (Document last updated on 8-Jun-14)
+Version: 1.0.1 (Document last updated on 9-Jun-14)
 */
 
 
@@ -16,19 +16,29 @@ var modalwindow = function() {
     var close = [].slice.call( document.querySelectorAll('.md-x, .md-overlay') );
     var ex = document.querySelector('.md-x');
 
+    // Open by clicking button
     open.forEach(function(el) {
         el.addEventListener('click', function() {
             modal.classList.add('md-show');
         });
     });
-
+    
+    // Close by clicking close[x] button and/or overlay
     close.forEach(function(el) {
         el.addEventListener('click', function() {
             modal.classList.remove('md-show');
             ex.classList.remove('md-show');
         });
     });
+    
+    // Close by pressing Esc
+    document.addEventListener('keydown', function(e) {
+        if ( e.keyCode == 27 ) {
+            modal.classList.remove('md-show');
+        }
+    });
 
+    // Reveal close[x] button
     modal.addEventListener('mouseover', function() {
         ex.classList.add('md-show');
     });
